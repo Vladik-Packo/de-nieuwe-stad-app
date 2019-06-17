a2a3209b32e1d2c1984b2240d4027a2cecb36425
Bow <1993bow@gmail.com>
2019-06-17 16:45:43 +0200

    punten toegevoegt, variables en berekening gemaakt

--- www/js/map.js
+++ www/js/map.js
@@ -88,10 +88,26 @@ document.getElementById("map").onload = function() {
 // get width and hight of svg
     var kX = document.getElementById('map').offsetWidth;
     var kY = document.getElementById('map').offsetHeight;
+
     var point = document.getElementById('point');
+    var point2 = document.getElementById('point2');
 
+    var loc = document.getElementById('loc');
 
+    var gx1 = 52.16105;
+    var gy1 = 5.37796;
 
+    var gx2 = 52.16043;
+    var gy2 = 5.38119;
+
+    var kx1 = kX/5;
+    var ky1 = kY/4;
+
+    var kx2 = kX/2;
+    var ky2 = kY/2;
+
+    var gpsx = 52.15996;
+    var gpsy = 5.379383;
 
 
     // Berekening locatie op map
@@ -103,13 +119,25 @@ document.getElementById("map").onload = function() {
     // Kx1+(gpsX-Gx1)*(Kx2-Kx1)/(Gx2-Gx1) = nx
     // Ky1+(gpsY-Gy1)*(Ky2-Ky1)/(Gy2-Gy1) = ny
 
-    console.log(gpsX + " lel");
+    var nx = kx1+(gpsx-gx1)*(kx2-kx1)/(gx2-gx1);
+    var ny = ky1+(gpsy-gy1)*(ky2-ky1)/(gy2-gy1);
+
+    loc.style.left = nx + "px";
+    loc.style.top = ny + "px";
+
+    console.log(nx + ' nx');
+    console.log(kx1 + ' kx1');
+    console.log(kx2 + ' okx2');
 
 
-    //point.style.left = width/2 + "px";
 
-    console.log(width);
-    console.log(height);
+
+    point.style.left = kX/5 + "px"; // == 52.16105
+    point.style.top = kY/4 + "px"; // == 5.37796
+
+    point2.style.left = kX/2 + "px"; // == 52.16043
+    point2.style.top = kY/2 + "px"; // == 5.38119
+
     var test = console.log(width);
 
 };
