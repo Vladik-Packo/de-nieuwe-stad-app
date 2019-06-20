683239b48024446d1e05d7aaff24f5d735b67b9f
Nuro <nuro0ok@gmail.com>
2019-06-20 13:13:51 +0200

    YOLO

--- www/js/agenda.js
+++ www/js/agenda.js
@@ -29,10 +29,9 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
-function Business() {
-    getJSON('http://145.120.239.63:8000/api/businesses',
+function Events() {
+    getJSON('http://145.120.239.36:8000/api/events',
         function(err, data) {
-
             if (err !== null) {
                 alert('Something went wrong: ' + err);
             } else {
@@ -40,23 +39,23 @@ function Business() {
 
                     let newDiv = document.createElement('div');
                     newDiv.className = "row";
-                    // document.getElementsByClassName("container")[i].appendChild(newDiv);
+                    document.getElementsByClassName("container")[i].appendChild(newDiv);
 
-                    // let divTitle = document.createElement('div');
-                    newDiv.className = "col s12 collapsible-header waves-effect waves-light waves-yellow btn-large transparent border-buttons";
-                    document.getElementsByClassName('row')[i+1].appendChild(newDiv);
-                    newDiv.innerHTML = data[i]['name'];
+                    let divTitle = document.createElement('div');
+                    divTitle.className = "collapsible-header col s12 waves-effect waves-light waves-yellow btn-large transparent border-buttons";
+                    document.getElementsByClassName('row')[i+1].appendChild(divTitle);
+                    divTitle.innerHTML = data[i]['name'];
                     console.log(data[i]['name']);
 
-                    // let divInfo = document.createElement('div');
-                    // divInfo.className = "col s12 collapsible-body flow-text panel grey lighten-2 z-depth-1";
-                    // document.getElementsByClassName('div')[i+1].appendChild(divInfo);
-                    // divInfo.innerHTML = data[i]['info'];
+                    let divInfo = document.createElement('div');
+                    divInfo.className = "col s12 collapsible-body flow-text panel grey lighten-2 z-depth-1";
+                    // document.getElementsByClassName('row')[i+1].appendChild(divInfo);
+                    divInfo.innerHTML = data[i]['business'];
 
-                    // let moreInfo = document.createElement('a');
-                    // moreInfo.href="#";
-                    // moreInfo.innerHTML = "<br> link";
-                    // document.getElementsByClassName('col s12 flow-text panel grey lighten-2 z-depth-1')[i].appendChild(moreInfo);
+                    let moreInfo = document.createElement('a');
+                    moreInfo.href="#";
+                    moreInfo.innerHTML = "<br> link";
+                    document.getElementsByClassName('col s12 flow-text panel grey lighten-2 z-depth-1')[i].appendChild(moreInfo);
 
             }
                 $(".flip").click(function(){
@@ -65,4 +64,4 @@ function Business() {
         }
     });
 }
-Business();
+Events();
